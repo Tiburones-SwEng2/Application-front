06bebaae44fed30116164b6c5fef93dfd4906c99
CesarSarmiento1111 <CesarSarmiento1111@users.noreply.github.com>
2025-06-13 13:40:31 -0500

    Añadir enrutamiento en App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DonationPage from './components/DonationPage'
+import ProductList from './components/ProductList'
 //import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <DonationPage />
-   
-    </>
+    <Routes>
+      <Route path="/" element={<DonationPage />} />
+      <Route path="/donaciones" element={<ProductList />} />
+    </Routes>
   )
 }
 
